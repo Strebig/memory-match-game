e306ea48323fb567df4f996e96e570fbc2918e94
Nick Strebig <njstrebig@gmail.com>
2019-04-04 23:46:24 -0700

    Functionality working Dynamically added content Randomized cards Added bootstrap and am playing around with that Testing new CSS/Visual enhancements

--- main.js
+++ main.js
@@ -1,78 +1,117 @@
 $(document).ready(initializeApp);
 
+var first_card_clicked = null;
+var second_card_clicked = null;
+var first_card_clicked_src = '';
+var second_card_clicked_src = '';
+var total_possible_matches = 2;
+var match_counter = 0;
+var canClickCard = true;
+var images = [
+    'images/johnsnow.jpg',
+    'images/dragonlady.jpg',
+    'images/arya.jpg',
+    'images/cerlannister.jpg',
+    'images/jaime.jpg',
+    'images/missingbody.jpg',
+    'images/redbeard.jpg',
+    'images/redlady.jpg',
+    'images/tryion.jpg'
+
+];
+
 function initializeApp() {
-    var first_card_clicked = null;
-    var second_card_clicked = null;
-    var total_possible_matches = 2;
-    var match_counter = 0;
-    var canClickCard = true;
-    var images = ['johnsnow.jpg', 'dragonlady.jpg'];
 
+    randomizeCards();
 
-    function randomizeCards (){
-        var doubleImages = images.concat(images);
-
-        for (var i = 0; i < doubleImages.length; i++) {
-
-            var container = $('<div>').addClass('cardContainer');
-            var card = $('<div>').addClass('card');
-            var back = $('<div>').addClass('back');
-            var front = $('<div>').addClass('front');
-            var image = $('<img>').addClass('imageMod').attr('src', doubleImages[i]);
-            front.append(image);
-            card.append(front,back);
-            container.append(card);
-            $('.mainGame').append(container);
-        }
-
-    }
-
-    $('.card').on('click', '.back', function cardClicked (event) {
+    $('.card').on('click', function cardClicked (event) {
         if (canClickCard === false){
             return;
         }
-// event current target is core javascript... Jquery cant do that
+        //Hide the back of the card
+        $(event.currentTarget).find('.back').hide();
+        $(event.currentTarget).find('.front').show();
+
         if (first_card_clicked == null) {
             first_card_clicked = event.currentTarget;
-            $(this).hide('.back');
-            first_card_clicked.find('.front img').attr('src');
-            return first_card_clicked;
+            first_card_clicked_src = $(first_card_clicked).find('.front img').attr('src');
+            return;
         }
-        second_card_clicked = event.currentTarget;
-        second_card_clicked.find('.front img').attr('src');
+        else {
+            second_card_clicked = event.currentTarget;
+            second_card_clicked_src = $(second_card_clicked).find('.front img').attr('src');
+        }
 
+        canClickCard = false;
 
-
-        if (first_card_clicked === second_card_clicked) {
+        if (first_card_clicked_src === second_card_clicked_src) {
             match_counter++;
             first_card_clicked = null;
             second_card_clicked = null;
+            canClickCard = true;
             if (match_counter === total_possible_matches) {
-                return "You have survived for now... but Winter is coming."
+                //window.alert("You have survived for now... but Winter is coming.")
             }
         }
-        canClickCard = false;
-        setTimeout(hideBothCards, 2000);
-
-
-        function hideBothCards() {
-            first_card_clicked = $('.back').show();
-            second_card_clicked = $('.back').show();
-            first_card_clicked = null;
-            second_card_clicked = null;
-            canClickCard = true;
+        else {
+            setTimeout(hideBothCards, 2000);
         }
 
     })
 
+
 }
 
-//pseudo code // .find('.front img').attr('src');
-//target front of the card to match the same card, and if they match..
-// stay front-faced. If they aren't a match, flip backwards.
-// only can choose 2 cards at a time.
-// Later: if they match... they dissolve
-//once all are matched... you win the game, animation screen appears
-//if you do not match them all after x amount of fails,
-// the night king takes over the screen and turns you into a night walker
+function randomizeCards () {
 
+    images = images.concat(images);
+    var shuffledImages = shuffle(images);
+    var container = $('<div>').addClass('row');
+
+    for (var i = 0; i < shuffledImages.length; i++) {
+
+        var card = $('<div>').addClass('card col-md-4');
+        var back = $('<div>').addClass('back');
+        var front = $('<div>').addClass('front');
+        var image = $('<img>').attr('src', images[i]);
+        var backImage = $('<img>').attr('src', 'images/gotlogo.jpg');
+        front.append(image);
+        back.append(backImage);
+        card.append(front);
+        card.append(back);
+        container.append(card);
+
+    }
+    container.find('.front').hide();
+    $('.mainGame').append(container);
+}
+
+
+function hideBothCards() {
+
+    $(first_card_clicked).find('.back').show();
+    $(second_card_clicked).find('.back').show();
+
+    first_card_clicked = null;
+    second_card_clicked = null;
+    canClickCard = true;
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
