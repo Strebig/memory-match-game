00d848f768dfea907c77170d035d93487554186c
Strebig <strebig.nick@gmail.com>
2019-07-12 01:22:33 +0000

    Completed Remaking of Memory Match Game Deleted header image Fixed the navbar / added reset button , connected about to my website Fixed scrolling sidebar at the bottom Added attempts remaining count down per game Uploaded a cleaner looking Game of Thrones card image Fixed the major bugs dealing with Game win / Loss conditions Fixed First Card Clicked / Second Card clicked bugs when cards were matched Made hovering effects on available move cards, disabled hover when a card is chosen or matched Made a Losing condition flip all cards over and unclickable, for the user to see where they all were before resetting

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
 $(document).ready(initializeApp);
 
-var first_card_clicked = null;
-var second_card_clicked = null;
-var first_card_clicked_src = null;
-var second_card_clicked_src = null;
-var total_possible_matches = 10;
-var match_counter = 0;
-var canClickCard = true;
-var images = [
+let first_card_clicked = null;
+let second_card_clicked = null;
+let first_card_clicked_src = null;
+let second_card_clicked_src = null;
+const total_possible_matches = 9;
+let match_counter = 0;
+let canClickCard = true;
+let gamesPlayed = 0;
+let accuracy = 0;
+let attempts = 0;
+let attemptsRemaining = 15;
+let wins = 0;
+const images = [
     'images/johnsnow.jpg',
     'images/dragonlady.jpg',
     'images/arya.jpg',
-    'images/cerlannister.jpg',
     'images/jaime.jpg',
     'images/missingbody.jpg',
     'images/redbeard.jpg',
     'images/redlady.jpg',
     'images/tryion.jpg',
     'images/nightwalker.jpg'
-
 ];
-var gamesPlayed = 0;
-var accuracy = 0;
-var attempts = 0;
-var wins = 0;
 
 function initializeApp() {
-
     randomizeCards();
-
     $('.resetButton').on('click', function () {
         gamesPlayed++;
         resetStats();
@@ -41,12 +38,14 @@ function initializeApp() {
 function displayStats() {
     $('.games-played .value').text(gamesPlayed);
     $('.attempts .value').text(attempts);
+    $('.attemptsRemaining .value').text(attemptsRemaining);
     $('.accuracy .value').text(accuracy + "%");
 }
 
 function resetStats() {
     accuracy = 0;
     attempts = 0;
+    attemptsRemaining = 15;
     wins = 0;
     canClickCard = true;
     first_card_clicked = null;
@@ -60,7 +59,7 @@ function randomizeCards() {
     shuffledImages = shuffle(shuffledImages);
     var container = $('<div>').addClass('row');
     for (var i = 0; i < shuffledImages.length; i++) {
-        var col = $('<div>').addClass('col-md-3 text-center mb-3');
+        var col = $('<div>').addClass('col-4 col-sm-3 col-lg-2 text-center mb-3 cardContainer');
         var card = $('<div>').addClass('got-card');
         var back = $('<div>').addClass('back');
         var front = $('<div>').addClass('front flip-scale-up-ver');
@@ -79,47 +78,62 @@ function randomizeCards() {
     $('.got-card').on('click', function cardClicked (event) {
         if (canClickCard === false){
             return;
+        } 
+        else {
+            $(event.currentTarget).find('.back').hide();
+            $(event.currentTarget).find('.front').show();
         }
-        $(event.currentTarget).find('.back').hide();
-        $(event.currentTarget).find('.front').show();
 
-        if (first_card_clicked == null) {
+        if (first_card_clicked === null && $(event.currentTarget).find('.got-card').prevObject[0].className === 'got-card') {
             first_card_clicked = event.currentTarget;
             first_card_clicked_src = $(first_card_clicked).find('.front img').attr('src');
+            $(first_card_clicked).removeClass('got-card');
             return;
-        }
-        else if (first_card_clicked === event.currentTarget)  {
+        } 
+        else if (first_card_clicked === event.currentTarget || $(event.currentTarget).find('.got-card').prevObject[0].className !== 'got-card') {
             return;
         }
         else {
             second_card_clicked = event.currentTarget;
             second_card_clicked_src = $(second_card_clicked).find('.front img').attr('src');
+            $(second_card_clicked).removeClass('got-card');
         }
         canClickCard = false;
         attempts++;
+        attemptsRemaining--;
         if (first_card_clicked_src === second_card_clicked_src) {
             match_counter++;
             wins++;
             first_card_clicked = null;
             second_card_clicked = null;
+            first_card_clicked_src = null;
+            second_card_clicked_src = null;
             canClickCard = true;
-            if (match_counter === total_possible_matches) {
-                $("#win-game-modal").modal('show');
+        } else {
+            if (attempts < 15) {
+                setTimeout(hideBothCards, 2000);
             }
         }
-        else if (attempts === 17) {
+
+        if (match_counter === total_possible_matches) {
+            $("#win-game-modal").modal('show');
+        }
+        else if (attempts === 15) {
             setTimeout(function () {
                 $("#lost-game-modal").modal('show');
-                gamesPlayed++;
-                resetStats();
-                displayStats();
-                $('.mainGame').empty();
-                randomizeCards();
-            }, 1000);
+                for (i = 0; i < shuffledImages.length; i++) {
+                    $(container).find('.front').show();
+                    $(container).find('.back').hide();
+                    canClickCard = false;
+                }
+                // gamesPlayed++;
+                // resetStats();
+                // displayStats();
+                // $('.mainGame').empty();
+                // randomizeCards();
+            }, 1500);
         }
-        else {
-            setTimeout(hideBothCards, 2000);
-        }
+
         accuracy = Math.ceil((wins / attempts) * 100);
         displayStats();
 
@@ -128,6 +142,8 @@ function randomizeCards() {
 
 
 function hideBothCards() {
+    $(first_card_clicked).addClass('got-card');
+    $(second_card_clicked).addClass('got-card');
 
     $(first_card_clicked).find('.back').show();
     $(second_card_clicked).find('.back').show();
@@ -144,7 +160,6 @@ function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
     while (0 !== currentIndex) {
-
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
         temporaryValue = array[currentIndex];
